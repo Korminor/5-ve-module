9896417fc180a15783521f3e494eacdce25b30ef
Eugene <BakulinEU@gmail.com>
2024-10-17 00:02:12 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,76 @@
+class House():
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        if isinstance(other.number_of_floors, int) and isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors = self.number_of_floors + value
+            return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def go_to(self, new_floor):
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor + 1):
+                print(i)
+
+
+house_1 = House('ЖК Жемчужный', 15)
+house_2 = House('ЖК Отрада', 5)
+
+print(house_1)
+print(house_2)
+print()
+print(house_1 == house_2) # __eq__
+print()
+house_1 = house_1 + 10 # __add__
+print(house_1)
+print(house_1 == house_2)
+print()
+house_1 += 10 # __iadd__
+print(house_1)
+print()
+house_2 = 10 + house_2 # __radd__
+print(house_2)
+print()
+print(house_1 > house_2)
+print(house_1 >= house_2)
+print(house_1 < house_2)
+print(house_1 <= house_2)
+print(house_1 != house_2)
